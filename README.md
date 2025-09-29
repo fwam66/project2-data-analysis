b5044f580da24753b17678ac97edab86b763de3c
fwam66 <201590583+fwam66@users.noreply.github.com>
2025-09-29 13:57:03 +1000

    read in CSV

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # project2-data-analysis
+What distinct household travel-behaviour profiles exist in the dataset VISTA, and how do these profiles differ by household context and home region, and between weekday and weekend? 
+
+Distinct household travel-behaviour profiles: public/private, trip duration categorised, number of stops, 
+
+Household contexts, home region: LGA, age, income, size of hh, dwelling type and owned, totalbikes
+
+Weekday and weekend 
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+import pandas as pd
+
+# Read in CSV files
+household = pd.read_csv('household_vista_2023_2024.csv')
+trips = pd.read_csv('trips_vista_2023_2024.csv')
+
+# Removes missing data that won't contribute to analysis
+household = household[household['hhinc_group'].notna()]
+
+# Split hhinc columns into Weekly and Annual income
+# Easier to read and understand
+household[['weekly_hhinc_group', 'annual_hhinc_group']] = household['hhinc_group'].str.split(' ', n=1, expand=True)
+household['annual_hhinc_group'] = household['annual_hhinc_group'].str.strip('()')
+household = household.drop(columns=['hhinc_group'])
+
+# Label encode income groups
+print(household['weekly_hhinc_group'].unique())
+
+
